0c0e627046eb31a84c69d7d967559dce391aa241
ewoudje <7384674+ewoudje@users.noreply.github.com>
2022-12-14 21:42:31 +0100

    Magneting Them

--- common/src/main/kotlin/net/takeoff/ship/TakeoffMagnets.kt
+++ common/src/main/kotlin/net/takeoff/ship/TakeoffMagnets.kt
@@ -14,59 +14,53 @@ import org.valkyrienskies.core.impl.api.ServerShipUser
 import org.valkyrienskies.core.impl.api.ShipForcesInducer
 import org.valkyrienskies.core.impl.api.Ticked
 import org.valkyrienskies.mod.common.util.toJOML
+import java.util.concurrent.CopyOnWriteArrayList
 
 @OptIn(InternalCoroutinesApi::class)
 class TakeoffMagnets(@JsonIgnore override var ship: ServerShip?) : ShipForcesInducer, ServerShipUser, Ticked {
-    private val magnets = mutableListOf<Magnet>()
+    private val magnets = CopyOnWriteArrayList<Magnet>()
 
     override fun applyForces(physShip: PhysShip) {
         val ship = ship as ServerShip
 
-        synchronized(magnets) {
-            magnets.forEach {
-                val (pos, otherMagnets) = it
+        magnets.forEach {
+            val (pos, otherMagnets) = it
 
-                val tPos = ship.shipToWorld.transformPosition(Vector3d(pos).add(0.5, 0.5, 0.5))
+            val tPos = it.ship.shipToWorld.transformPosition(Vector3d(pos).add(0.5, 0.5, 0.5))
 
-                otherMagnets.forEach { other ->
-                    val tPosOther = other.lastWorldPos
+            otherMagnets.forEach { other ->
+                val tPosOther = other.ship.shipToWorld.transformPosition(Vector3d(other.pos).add(0.5, 0.5, 0.5))
 
-                    var dist = tPosOther.distanceSquared(tPos)
-                    if (dist > MAGNET_DISTANCE) return
-                    if (dist < 0.1) dist = 0.1
+                var dist = tPosOther.distanceSquared(tPos)
+                if (dist > MAGNET_DISTANCE) return
+                if (dist < 0.1) dist = 0.1
 
-                    val power = (MAGNET_POWER / 100) / dist
-                    val force = Vector3d(tPos).sub(tPosOther).normalize(-power)
+                val power = MAGNET_POWER / dist
 
-                    //force.negate()
+                //if (it.north == other.north) power = -power
 
-                    //if (it.north == other.north) force.negate()
+                val force = Vector3d(tPos).sub(tPosOther).normalize(-power)
 
-                    physShip.applyInvariantForceToPos(force, Vector3d(tPos).sub(ship.transform.positionInWorld))
-                }
+                physShip.applyInvariantForceToPos(force, Vector3d(tPos).sub(ship.transform.positionInWorld))
             }
         }
     }
 
     fun addMagnet(pos: BlockPos, north: Boolean) {
-        synchronized(magnets) {
-            val magnet = Magnet(pos.toJOML(), mutableListOf(), north)
-            magnets.add(magnet)
-            allMagnets.add(magnet)
-        }
+        val magnet = Magnet(pos.toJOML(), mutableListOf(), north, ship = ship!!)
+        magnets.add(magnet)
+        allMagnets.add(magnet)
     }
 
     fun removeMagnet(bpos: BlockPos) {
-        synchronized(magnets) {
-            val pos = bpos.toJOML()
-            magnets.removeIf { it.pos == pos }
-            allMagnets.removeIf { it.pos == pos }
-        }
+        val pos = bpos.toJOML()
+        magnets.removeIf { it.pos == pos }
+        allMagnets.removeIf { it.pos == pos }
     }
 
     companion object {
         val MAGNET_DISTANCE = 10 * 10
-        val MAGNET_POWER = 100.0 * 100.0
+        val MAGNET_POWER = 600.0 * 600.0
 
         fun getOrCreate(ship: ServerShip): TakeoffMagnets =
             ship.getAttachment<TakeoffMagnets>()
@@ -75,19 +69,24 @@ class TakeoffMagnets(@JsonIgnore override var ship: ServerShip?) : ShipForcesInd
         private val allMagnets = mutableListOf<Magnet>()
     }
 
-    data class Magnet(val pos: Vector3i, @JsonIgnore val otherMagnets: MutableList<Magnet> = mutableListOf(), val north: Boolean, @JsonIgnore var lastWorldPos: Vector3d = Vector3d())
+    data class Magnet(
+        val pos: Vector3i,
+        @JsonIgnore var otherMagnets: List<Magnet> = mutableListOf(),
+        val north: Boolean,
+        @JsonIgnore var lastWorldPos: Vector3d = Vector3d(),
+        val ship: ServerShip
+    )
 
     override fun tick() {
-        synchronized(magnets) {
-            magnets.forEach {
-                it.lastWorldPos = ship!!.shipToWorld.transformPosition(Vector3d(it.pos).add(0.5, 0.5, 0.5))
-                it.otherMagnets.clear()
-                for (magnet in allMagnets) {
-                    if (!magnets.any { it.pos == magnet.pos } && (magnet.lastWorldPos.distanceSquared(it.lastWorldPos) < MAGNET_DISTANCE)) {
-                        it.otherMagnets.add(magnet)
-                    }
+        magnets.forEach {
+            it.lastWorldPos = ship!!.shipToWorld.transformPosition(Vector3d(it.pos).add(0.5, 0.5, 0.5))
+            val otherMagnets = mutableListOf<Magnet>()
+            for (magnet in allMagnets) {
+                if (magnet.ship.id != this.ship!!.id && (magnet.lastWorldPos.distanceSquared(it.lastWorldPos) < MAGNET_DISTANCE)) {
+                    otherMagnets.add(magnet)
                 }
             }
+            it.otherMagnets = otherMagnets
         }
     }
 }
